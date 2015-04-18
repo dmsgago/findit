eccbd9a2f375b3739d4e2f51a45f6f8932a6aa4d
Diego Martín <dmsgago@gmail.com>
2015-04-18 17:58:22 +0200

    Oauth configurado y listo para probarlo

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -18,6 +18,20 @@ def get_request_token():
     authorize_url = AUTHENTICATE_URL + TOKENS["request_token"]
     return template('index.tpl', authorize_url=authorize_url)
 
+# Función que devuelve a la aplicacion un token de acceso
+def get_access_token(TOKENS):
+    oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+                   resource_owner_key=TOKENS["request_token"],
+                   resource_owner_secret=TOKENS["request_token_secret"],
+                   verifier=TOKENS["verifier"],
+    )
+    r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+    TOKENS["access_token"] = credentials.get('oauth_token')[0]
+    TOKENS["access_token_secret"] = credentials.get('oauth_token_secret')[0]
+    return template('buscador.tpl')
+    
 # OAuth1
 REQUEST_TOKEN_URL = " https://api.twitter.com/oauth/request_token"
 AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token="
@@ -35,3 +49,22 @@ def index():
 def get_verifier():
     TOKENS["verifier"] = request.query.oauth_verifier
     get_access_token(TOKENS)
+
+# Carga el buscador
+@post('/map')
+def buscador_submit():
+    hashtag = request.forms.get("hashtag")
+    oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+                   resource_owner_key=TOKENS["access_token"],
+                   resource_owner_secret=TOKENS["access_token_secret"])
+ 
+    url = 'https://api.twitter.com/1.1/search/tweets.json?q='
+ 
+    r = requests.post(url=url+hashtag+'result_type=recent',
+                      data={"status":hashtag},
+                      auth=oauth)
+    if r.status_code == 200:
+        return template('mapa.tpl')
+    else:
+        return "<p>Hubo un problema en el buscador</p>"
---
+++ wsgi/views/buscador.tpl
@@ -0,0 +1,13 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>.::Usando la API de twitter::.</title>
+  </head>
+  <body>
+    <p>Busca un hashtag:</p>
+    <br />
+    <form action="/map" method="post">
+      <p><textarea name="hashtag" id="textbox" rows="3" cols="50"></textarea></p>
+      <p><input type="submit" class="button" value="Enviar" /></p>
+    </form>
+  </body>
+</html>
