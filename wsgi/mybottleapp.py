cd34d5433550690d29251d06a8488a54161109ec
Diego Martín <dmsgago@gmail.com>
2015-04-18 18:38:06 +0200

    Pruebas

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -7,11 +7,19 @@ TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/
 
 application=default_app()
 
+# OAuth1
+REQUEST_TOKEN_URL = " https://api.twitter.com/oauth/request_token"
+AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token="
+ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
+CONSUMER_KEY = "otZEhc6iU81FfeaY4PFs91y9N"
+CONSUMER_SECRET = "8ZspqHV33gQLeyHH2maeMLEiRKOgUBRY1TsdBVIL2PIFhxFzI7"
+TOKENS = {}
+
 
 # Funcion para obtener el token de solicitud y su secreto
 def get_request_token():
-    oauth = OAuth1(CONSUMER_KEY, client_secret=CONSUMER_SECRET)
-    r = requests.post(url=REQUEST_TOKEN_URL, auth=oauth)
+    oauth = OAuth1(CONSUMER_KEY,client_secret=CONSUMER_SECRET)
+    r = requests.post(url=REQUEST_TOKEN_URL,auth=oauth)
     credentials = parse_qs(r.content)
     TOKENS["request_token"] = credentials.get('oauth_token')[0]
     TOKENS["request_token_secret"] = credentials.get('oauth_token_secret')[0]
@@ -31,15 +39,6 @@ def get_access_token(TOKENS):
     TOKENS["access_token"] = credentials.get('oauth_token')[0]
     TOKENS["access_token_secret"] = credentials.get('oauth_token_secret')[0]
     return template('buscador.tpl')
-'''    
-# OAuth1
-REQUEST_TOKEN_URL = " https://api.twitter.com/oauth/request_token"
-AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token="
-ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
-CONSUMER_KEY = "otZEhc6iU81FfeaY4PFs91y9N"
-CONSUMER_SECRET = "8ZspqHV33gQLeyHH2maeMLEiRKOgUBRY1TsdBVIL2PIFhxFzI7"
-TOKENS = {}
-'''
 '''
 @route('/')
 def index():
