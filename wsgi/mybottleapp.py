29582ae886346dfa4c2b159fe5b4f03e9ad7f92e
Diego Martín <dmsgago@gmail.com>
2015-05-12 00:59:09 +0200

    Modificado el formulario de la plantilla buscador.tpl

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -67,7 +67,6 @@ def get_verifier():
 @post('/map')
 def findit():
     tipobusqueda = request.forms.get("TipoBusqueda")
-    return "<p>%s</p>" %tipobusqueda
     if tipobusqueda == "0":
         elementos["q"] = request.forms.get("hashtag")
         oauth = send_oauth(TOKENS)
