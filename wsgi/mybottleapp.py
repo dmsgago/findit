088b38838527e02ac3abf282a5dfe44b29c1b528
Diego Martín <dmsgago@gmail.com>
2015-05-14 09:22:17 +0200

    Modificado el programa principal para realizar la busqueda de geolocalizaciones por el nombre de usuario

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -100,7 +100,6 @@ def findit():
         r = requests.get(url, params=elementos, auth=oauth)        
         if r.status_code == 200:
             respuesta = r.json()
-            return ('<p> %s </p>'%respuesta)
                 # Recorre cada tuit
             for tweet in respuesta:
                 if tweet["geo"] != None:
