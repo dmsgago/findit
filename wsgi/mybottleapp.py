f30608b41d4ea9df2eca543d839fdd3792e8fa5d
Diego Martín <dmsgago@gmail.com>
2015-05-20 12:39:46 +0200

    Modificado el programa principal para que almacene información sobre el usuario que escribió el tuit y envía la información a la plantilla mapa.tpl

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -86,9 +86,9 @@ def findit():
                     if tweet["geo"]["coordinates"][0] != 0 and tweet["geo"]["coordinates"][1] != 0:
                         # Almacena la ubicacion en una lista
                         ubicaciones.append(tweet["geo"]["coordinates"])
-                tuit.append(tweet["text"])
-                nombreusuario.append(tweet["user"]["name"])
-                imagenperfil.append(tweet["user"]["profile_image_url"])
+                tuit.append(str(tweet["text"]))
+                nombreusuario.append(str(tweet["user"]["name"]))
+                imagenperfil.append(str(tweet["user"]["profile_image_url"]))
         else:
             return ('<p>JSON no obtenido.</p>')
 
@@ -113,9 +113,9 @@ def findit():
                     if tweet["geo"]["coordinates"][0] != 0 and tweet["geo"]["coordinates"][1] != 0:
                         # Almacena la ubicacion en una lista
                         ubicaciones.append(tweet["geo"]["coordinates"])
-                tuit.append(tweet["text"])
-                nombreusuario.append(tweet["user"]["name"])
-                imagenperfil.append(tweet["user"]["profile_image_url"])
+                tuit.append(str(tweet["text"]))
+                nombreusuario.append(str(tweet["user"]["name"]))
+                imagenperfil.append(str(tweet["user"]["profile_image_url"]))
         else:
             return ('<p>JSON no obtenido.</p>')
 
