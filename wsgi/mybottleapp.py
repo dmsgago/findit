13a1f4765940af980877bca85ae22c3f32cce9ca
Diego Martín <dmsgago@gmail.com>
2015-05-21 11:58:08 +0200

    Modificado el programa para corregir errores en la codificación

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -114,8 +114,8 @@ def findit():
                         # Almacena la ubicacion en una lista
                         ubicaciones.append(tweet["geo"]["coordinates"])
                         imagen = tweet["user"]["profile_image_url"]
-                        texto = str(tweet["text"].encode("utf-8"))
-                        nombre = str(tweet["user"]["name"].encode("utf-8"))
+                        texto = tweet["text"]
+                        nombre = tweet["user"]["name"].encode("utf-8")
                         tuit.append("<img src='"+imagen+"' style='float:left; padding: 5px;' /><strong>"+nombre+"</strong>: "+texto)
         else:
             return ('<p>JSON no obtenido.</p>')
