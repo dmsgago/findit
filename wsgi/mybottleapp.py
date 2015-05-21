7fe961e155643910620154012eabfddabe0f733c
Diego Martín <dmsgago@gmail.com>
2015-05-21 15:58:32 +0200

    Modificado el programa principal para que almacene información sobre el usuario que escribió el tuit y envía la información a la plantilla mapa.tpl

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -111,9 +111,9 @@ def findit():
                     if tweet["geo"]["coordinates"][0] != 0 and tweet["geo"]["coordinates"][1] != 0:
                         # Almacena la ubicacion en una lista
                         ubicaciones.append(tweet["geo"]["coordinates"])
-                        imagen = unicode(tweet["user"]["profile_image_url"])
-                        texto = unicode(tweet["text"])
-                        nombre = unicode(tweet["user"]["name"])
+                        imagen = tweet["user"]["profile_image_url"].encode("utf-8", "ignore")
+                        texto = tweet["text"].encode("utf-8", "ignore")
+                        nombre = tweet["user"]["name"]encode("utf-8", "ignore")
                         fecha = tweet["created_at"].encode("utf-8")
                         fecha = fecha[:19]
                         tuit.append("<img src='"+imagen+"' style='float:left; padding: 5px;' /><strong>"+nombre+"</strong>: "+texto+" ("+fecha+")")
